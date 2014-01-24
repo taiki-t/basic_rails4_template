93ec779f6a47f576a1c513e9979c9c744fc2dc47
tkymtk <no.one.of.us.is.as.smart.as.all.of.us@gmail.com>
2014-01-24 17:04:32 +0900

    modify spork

--- basic_rails4_template.rb
+++ basic_rails4_template.rb
@@ -110,6 +110,7 @@ gsub_file 'gemfile',/# Use sqlite3 as the database for Active Record\n.*'/, ''
 run 'bundle install'
 
 # bootstrap(twitter)
+# ------------------------------------
 create_file 'app/assets/stylesheets/custom.css.scss' ,'@import "bootstrap";'
 gsub_file 'app/assets/javascripts/Application.js', %r{//= require jquery_ujs}, <<EOF
 //= require jquery_ujs
@@ -151,6 +152,12 @@ EOF
 run 'bundle exec spork --bootstrap'
 
 gsub_file 'spec/spec_helper.rb', /require.*end/m, <<'EOF'
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+
+Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
@@ -197,7 +204,44 @@ gsub_file 'spec/spec_helper.rb', /require.*end/m, <<'EOF'
     #     --seed 1234
     config.order = "random"
   end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+end
+
+# --- Instructions ---
+# Sort the contents of this file into a Spork.prefork and a Spork.each_run
+# block.
+#
+# The Spork.prefork block is run only once when the spork server is started.
+# You typically want to place most of your (slow) initializer code in here, in
+# particular, require'ing any 3rd-party gems that you don't normally modify
+# during development.
+#
+# The Spork.each_run block is run each time you run your specs.  In case you
+# need to load files that tend to change during development, require them here.
+# With Rails, your application modules are loaded automatically, so sometimes
+# this block can remain empty.
+#
+# Note: You can modify files loaded *from* the Spork.each_run block without
+# restarting the spork server.  However, this file itself will not be reloaded,
+# so if you change any of the code inside the each_run block, you still need to
+# restart the server.  In general, if you have non-trivial code in this file,
+# it's advisable to move it into a separate file so you can easily edit it
+# without restarting spork.  (For example, with RSpec, you could move
+# non-trivial code into a file spec/support/my_helper.rb, making sure that the
+# spec/support/* files are require'd from inside the each_run block.)
+#
+# Any code that is left outside the two blocks will be run during preforking
+# *and* during each_run -- that's probably not what you want.
+#
+# These instructions should self-destruct in 10 seconds.  If they don't, feel
+# free to delete them.
+
 EOF
+
 
 # .gitignore
 # ----------------------------------------------
